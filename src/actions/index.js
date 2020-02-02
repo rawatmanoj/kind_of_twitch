2b55ed9f8a7016fe29387af856636ee764eb65c3
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-02-03 04:02:45 +0530

    adding

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -31,6 +32,8 @@ export const createStream = formValues => async (dispatch,getState) =>{
           type:CREATE_STREAM, 
           payload:response.data
       });
+
+      history.push('/');
 
 };
 
